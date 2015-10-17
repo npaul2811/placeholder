0192cf32d225b93b4975aeded73a642ba0ceff8f
Nitin Paul <npaul2811@gmail.com>
2015-10-17 13:42:37 +0530

    Version 1.0

---
+++ placeholder.py
@@ -0,0 +1,107 @@
+
+import sys, os, hashlib
+from django.conf import settings
+
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'f+9%@gf%=byo8k1@bmumg0vi3=ax@jvyvjvc@b4y64e+wziy#m')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
+BASE_DIR = os.path.dirname(__file__)
+
+settings.configure(
+	DEBUG = DEBUG,
+	SECRET_KEY = SECRET_KEY,
+	ALLOWED_HOSTS = ALLOWED_HOSTS,
+	ROOT_URLCONF = __name__,
+	MIDDLEWARE_CLASSES = (
+		'django.middleware.common.CommonMiddleware',
+		'django.middleware.csrf.CsrfViewMiddleware',
+		'django.middleware.clickjacking.XFrameOptionsMiddleware'
+	),	
+	INSTALLED_APPS = (
+		'django.contrib.staticfiles',
+	),
+	TEMPLATE_DIRS = (
+		os.path.join(BASE_DIR, 'templates'),
+	),
+	STATICFILES_DIRS = (
+		os.path.join(BASE_DIR, 'static'),
+	),
+	STATIC_URL = '/static/',
+)
+
+from django import forms
+from django.conf.urls import url
+from django.core.cache import cache
+from django.core.urlresolvers import reverse
+from django.core.wsgi import get_wsgi_application
+from django.http import HttpResponse as response, HttpResponseBadRequest
+from django.shortcuts import render
+from django.views.decorators.http import etag
+from io import BytesIO
+from PIL import Image, ImageDraw
+
+# Views go here
+#=================================================================
+def index(request):
+	example = reverse('placeholder', kwargs = {'width': 50, 'height': 50})
+	context = {
+		'example': request.build_absolute_uri(example)
+	}
+	return render(request, 'home.html', context)
+
+class ImageForm(forms.Form):
+	"""Form to validate requested placeholder image"""
+
+	height = forms.IntegerField(min_value = 1, max_value = 2000)
+	width = forms.IntegerField(min_value = 1, max_value = 2000)
+
+	def generate(self, image_format = 'PNG'):
+		"""Generate an image of the given type and return the raw image"""
+
+		height = self.cleaned_data['height']
+		width = self.cleaned_data['width']
+		key = '{}.{}.{}'.format(width, height, image_format)
+		content = cache.get(key)
+		if content is None:
+			image = Image.new('RGB', (width, height))
+			draw = ImageDraw.Draw(image)
+			text = '{} X {}'.format(width, height)
+			textwidth, textheight = draw.textsize(text)
+			if textwidth < width and textheight < height:
+				texttop = (height - textheight)
+				textleft = (width - textwidth)
+				draw.text((textleft, texttop), text, fill = (255, 255, 255))
+			content = BytesIO()
+			image.save(content, image_format)
+			content.seek(0)
+			cache.set(key, content, 60*60)
+		return content
+
+def generate_etag(request, width, height):
+	content = 'Placeholder: {0} x {1}'.format(width, height)
+	return hashlib.sha1(content.encode('utf-8')).hexdigest()
+
+@etag(generate_etag)
+def placeholder(request, width, height):
+	form = ImageForm({'height': height, 'width': width})
+	if form.is_valid():
+		image = form.generate()
+		return response(image, content_type = 'image/png')
+	else:
+		return HttpResponseBadRequest("Invalid Image Request")
+
+# URL patterns go here
+#=================================================================
+urlpatterns = [
+	url(r'^$', index, name = 'homepage'),
+	url(r'^image/(?P<width>[0-9]+)x(?P<height>[0-9]+)/$', placeholder, name = 'placeholder'),
+]
+
+application = get_wsgi_application()
+
+if __name__ == "__main__":
+	from django.core.management import execute_from_command_line
+	execute_from_command_line(sys.argv)
